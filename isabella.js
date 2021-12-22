1f326189ee5482a6ff057a73fd7c5948f33d32e1
Isabella Burin <iburin@myseneca.ca>
2021-12-21 21:14:27 -0500

    added for loop with 10 outputs

--- isabella.js
+++ isabella.js
@@ -1,1 +1,5 @@
-console.log("Change me to say something cool 10 times!");
+//console.log("Change me to say something cool 10 times!");
+
+for(var i = 0; i < 10; ++i){
+    console.log("something cool");
+}
